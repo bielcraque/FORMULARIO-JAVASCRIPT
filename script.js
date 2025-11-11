8397a0e78f438af605d247814613e40751377c7a
bielcraque <gabriel.reis.timoteo@escola.pr.gov.br>
2025-11-10 21:43:58 -0300

    Add form validation script with commented logic

---
+++ script.js
@@ -0,0 +1,26 @@
+const nome = document.querySelector("#name");
+const senha = document.querySelector("#senha");
+const form = document.querySelector("form");
+
+// form.addEventListener("submit", (e) => {
+//   let enviarForm = true;
+
+//   if (!nome.value) {
+//     console.log("O nome está vazio");
+//     enviarForm = false;
+//   }
+
+//   if (!senha.value) {
+//     console.log("A senha está vazia");
+//     enviarForm = false;
+//   }
+
+//   if (senha.value.length < 4 || senha.value.length > 10) {
+//     console.log("A senha tem menos do que 4 ou mais do que 10 caracteres");
+//     enviarForm = false;
+//   }
+
+//   if (!enviarForm) {
+//     e.preventDefault();
+//   }
+// });
